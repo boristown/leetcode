babc53844f41176b0ae5b416ee31d06da71a71a0
Boris Town <boristown@outlook.com>
2021-10-31 21:24:53 +0800

    Trie

--- algorithm.py
+++ algorithm.py
@@ -28,7 +28,37 @@ class UnionFind:
     def connected(self, x: int, y: int) -> bool:
         x, y = self.findset(x), self.findset(y)
         return x == y
-        
+
+class Trie:
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEnd = False
+    
+    def searchPrefix(self, prefix: str) -> "Trie":
+        node = self
+        for ch in prefix:
+            ch = ord(ch) - ord("a")
+            if not node.children[ch]:
+                return None
+            node = node.children[ch]
+        return node
+
+    def insert(self, word: str) -> None:
+        node = self
+        for ch in word:
+            ch = ord(ch) - ord("a")
+            if not node.children[ch]:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self.searchPrefix(word)
+        return node is not None and node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        return self.searchPrefix(prefix) is not None
+
 def prefix2D(matrix):
     '''
     二维前缀和
