f51e8af00a36cccac19a94ad10ea17dbf140eacb
Boris Town <boristown@outlook.com>
2021-11-04 23:02:56 +0800

    正则表达式

--- context.py
+++ context.py
@@ -1,5 +1,9 @@
 import algorithm
+import re
 
 if __name__ == "__main__":
     print(algorithm.LCS("abc","ebvcy"))
     print(algorithm.largestRectangleArea([2,1,2]))
+    pat = "^(.+?\D)?0\d"
+    print(not re.match(pat,"1+051"))
+    print(not re.match(pat,"05+1"))
