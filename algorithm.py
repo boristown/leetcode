3faffd814cdf21d9f865399ac55f5488c5c4cfa5
AI Era <47687653+boristown@users.noreply.github.com>
2021-11-26 19:29:08 +0800

    binarySearch

--- algorithm.py
+++ algorithm.py
@@ -307,3 +307,33 @@ class bfs_utils_2D:
                     seen.add((a,b))
                     Q.append((step1,a,b))
         return -1
+
+class binarySearch:
+    '''
+    二分查找工具包
+    '''
+    @staticmethod
+    def bisect_right(l,r,func):
+        '''
+        右侧查找
+        '''
+        while l<r:
+            mid=(l+r)//2
+            if func(mid):
+                r=mid
+            else:
+                l=mid+1
+        return l
+
+    @staticmethod
+    def bisect_left(l,r,func):
+        '''
+        左侧查找
+        '''
+        while l<r:
+            mid=(l+r)//2+1
+            if func(mid):
+                l=mid
+            else:
+                r=mid-1
+        return l
