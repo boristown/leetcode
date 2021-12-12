a28e5ec60e790f9639e977993e745f9e59a2ea2e
Boris Town <boristown@outlook.com>
2021-12-12 09:10:50 +0800

    maxDConnectedArea

--- algorithm.py
+++ algorithm.py
@@ -415,3 +415,23 @@ class GraphTheory:
                     return True
                 UF.unite(i,j)
         return False
+    
+    @staticmethod
+    def maxDConnectedArea(G):
+        '''
+        计算有向图的最大连通区域
+        :param G:example [[set(),set([1,2])],[set(0),set()],[set(0),set()]]
+        '''
+        n=len(G)
+        def dp(i):
+            if G[i][1]:
+                for j in G[i][1]:
+                    if j not in vis:
+                        vis.add(j)
+                        dp(j)
+        ans=0
+        for i in range(n):
+            vis = {i}
+            dp(i)
+            ans=max(ans,len(vis))
+        return ans
