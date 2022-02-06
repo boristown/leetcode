4ea12aa7f290530f05c6bb54d06be936d239a14d
AI Era <47687653+boristown@users.noreply.github.com>
2022-02-06 16:49:43 +0800

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -47,6 +47,29 @@ class UnionFind:
             self.size[x] = 1
             self.setCount += 1
 
+#优先队列
+class PQ:
+    def __init__(self,Q,big):
+        self.Q = Q
+        self.big = big
+        if big:
+            for i in range(len(Q)):
+                Q[i] *= -1
+        heapq.heapify(self.Q)
+
+    def push(self,it):
+        if self.big:
+            heapq.heappush(self.Q,-it)
+        else:
+            heapq.heappush(self.Q,it)
+    
+    def pop(self):
+        it = heapq.heappop(self.Q)
+        if self.big:
+            return -it
+        else:
+            return it
+
 class Trie:
     def __init__(self):
         self.children = [None] * 26
