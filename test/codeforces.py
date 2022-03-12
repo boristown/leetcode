e3b78672f1a20508e6c57bee0b787136ff197fc1
Boris Town <boristown@outlook.com>
2022-03-12 21:26:26 +0800

    Update codeforces.py

--- test/codeforces.py
+++ test/codeforces.py
@@ -1,14 +1,12 @@
 from collections import *
 INF = float("inf")
-n = int(input())
-L = list(map(int,input().split()))
-def calculate(n,L):
-    cnt = Counter()
-    for i in range(n):
-        cnt[L[i]%2] += 1
-    d = cnt.most_common(1)[0][0]
-    for i in range(n):
-        if L[i]%2 != d:
-            return i+1
 
-print(calculate(n,L))
+def process(n,m,M):
+    return 0
+
+n,m = map(int,input().split())
+M = []
+for i in range(n):
+    L = list(map(int,input().split()))
+    M.append(L)
+print(process(n,m,M))
