70fc0d7af99ff4b518513147b8cda96adc620d6e
AI Era <47687653+boristown@users.noreply.github.com>
2022-03-16 09:01:24 +0800

    Create SortedCounter.py

---
+++ SortedCounter.py
@@ -0,0 +1,43 @@
+from sortedcontainers import *
+from collections import *
+
+class SortedCounter:
+
+    def __init__(self):
+        self.C = Counter()
+        self.S = SortedList()
+        
+    def inc(self, key: str) -> None:
+        self.C[key]+=1
+        n=self.C[key]
+        if n>=1:
+            self.S.discard((n-1,key))
+        self.S.add((n,key))
+        
+    def dec(self, key: str) -> None:
+        self.C[key]-=1
+        n=self.C[key]
+        self.S.discard((n+1,key))
+        if n:
+            self.S.add((n,key))
+
+    def getMaxKey(self) -> str:
+        return self.S[-1][1] if self.S else ""
+
+    def getMinKey(self) -> str:
+        return self.S[0][1] if self.S else ""
+
+    def most_common(self,n=0) -> list:
+        if n:
+            return list(self.S[:n])
+        else:
+            return list(self.S)
+
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
