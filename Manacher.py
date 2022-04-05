2dc0e27c7e8e74d2addf2b5be3123a771b863689
AI Era <47687653+boristown@users.noreply.github.com>
2022-04-05 22:39:31 +0800

    Update Manacher.py

--- Manacher.py
+++ Manacher.py
@@ -1,41 +1,45 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#最长回文子串—Manacher 算法 及 python实现
-#https://www.cnblogs.com/dahu-daqing/p/9302681.html
+class Manacher:
+    '''
+    马拉车算法O(n)
+    计算字符串的所有回文信息
+    https://oi-wiki.org/string/manacher/
+    '''
+    def __init__(self,s):
+        self.s = s
+        self.n = len(s)
+    
+    def get_odd(self):
+        '''
+        获取每个中心点的奇回文半径
+        '''
+        # Python Version
+        d1 = [0] * self.n
+        l, r = 0, -1
+        for i in range(0, self.n):
+            k = 1 if i > r else min(d1[l + r - i], r - i + 1)
+            while 0 <= i - k and i + k < self.n and self.s[i - k] == self.s[i + k]:
+                k += 1
+            d1[i] = k
+            k -= 1
+            if i + k > r:
+                l = i - k
+                r = i + k
+        return d1
 
-def max_substr(string):
-  s_list = [s for s in string]
-  string = '#' + '#'.join(s_list) + '#'
-  max_length = 0
-  length = len(string)
-  for index in range(0, length):
-    r_length = get_length2(string, index, max_length)
-    if max_length < r_length:
-      max_length = r_length
-  return max_length
-
-def get_length2(string, index, max_length):
-  # 基于已知的最长字串求最长字串
-  # 1.中心+最大半径超出字符串范围, return
-  r_ = len(string)
-  if index + max_length > r_:
-    return max_length
-
-  # 2.无法超越最大半径, return
-  l_string = string[index - max_length + 1 : index + 1]
-  r_string = string[index : index + max_length]
-  if l_string != r_string[::-1]:
-    return max_length
-
-  # 3.计算新的最大半径
-  result = max_length
-  for i in range(max_length, r_):
-    if index-i >= 0 and index+i < r_ and string[index-i] == string[index+i]:
-      result += 1
-    else:
-      break
-  return result - 1
-
-if __name__ == "__main__":
-  result = max_substr("35534321")
-  print(result)
+    def get_even(self):
+        '''
+        获取每个中心点的偶回文半径
+        '''
+        # Python Version
+        d2 = [0] * self.n
+        l, r = 0, -1
+        for i in range(0, self.n):
+            k = 0 if i > r else min(d2[l + r - i + 1], r - i + 1)
+            while 0 <= i - k - 1 and i + k < self.n and self.s[i - k - 1] == self.s[i + k]:
+                k += 1
+            d2[i] = k
+            k -= 1
+            if i + k > r:
+                l = i - k - 1
+                r = i + k
+        return d2
