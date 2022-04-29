3a424b432378c0b4373ec902f3c3b99958af2b23
AI Era <47687653+boristown@users.noreply.github.com>
2022-04-29 14:47:35 +0800

    Update Dijkstra.py

--- Graph/ShortestPath/Dijkstra.py
+++ Graph/ShortestPath/Dijkstra.py
@@ -24,27 +24,3 @@ def dijkstra(graph,s):
                 ans[node2] = dis
                 heappush(Q,(dis,node2))
     return ans
-
-
-def dijkstra_path(graph,s):
-    '''
-    在有向正权图中：从s出发到所有节点的最短路路径
-    graph:defaultdict(dict)
-    '''
-    Q = [(0,s)]
-    last = {}
-    ans = defaultdict(lambda:float("inf"))
-    ans[s] = 0
-    vis = set()
-    while Q:
-        _, node = heappop(Q)
-        if node in vis:
-            continue
-        vis.add(node)
-        for node2 in graph[node]:
-            dis = ans[node] + graph[node][node2]
-            if ans[node2] > dis:
-                last[node2] = node
-                ans[node2] = dis
-                heappush(Q,(dis,node2))
-    return ans
