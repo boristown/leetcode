f57d0402d42e6886ea964596fcd19acf503f16cd
Boris Town <boristown@outlook.com>
2022-04-30 13:33:54 +0800

    Update _test_leetcode.py

--- _test_leetcode.py
+++ _test_leetcode.py
@@ -3,13 +3,16 @@ from Graph.ShortestPath.Dijkstra import dijkstra
 from Graph.visualize import visualize
 
 if __name__ == "__main__":
-    #
-    print("re:")
-    pat = "^(.+?\D)?0\d"
-    print(not re.match(pat,"1+051"))
-    print(not re.match(pat,"05+1"))
-    #
-    print("dijkstra:")
+    #模式匹配：正则表达式
+    print("模式匹配：正则表达式:")
+    s = "boristown@gmail.com"
+    pat = "^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
+    print("字符串："+s+"\n正则："+pat+"\n匹配结果："+str(re.match(pat,s)))
+    s = "leetcode.cn"
+    pat = "[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?"
+    print("字符串："+s+"\n正则："+pat+"\n匹配结果："+str(re.match(pat,s)))
+    #最短路：dijkstra
+    print("最短路：dijkstra:")
     e = {1:[(2,1),(3,3),(5,5)],2:[(4,5)],3:[(4,2),(6,7)],4:[(6,1)],5:[],6:[]}
     print(visualize(e))
     dis = dijkstra(e,1)
