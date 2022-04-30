76b814027a55c92d097e2d43440458e3144609d5
Boris Town <boristown@outlook.com>
2022-04-30 14:29:47 +0800

    Update main_leetcode.py

--- main_leetcode.py
+++ main_leetcode.py
@@ -15,26 +15,24 @@ if __name__ == "__main__":
     pat = "[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?"
     print("字符串："+s+"\n正则："+pat+"\n匹配结果："+str(re.match(pat,s)))
     #正权最短路：dijkstra
-    print("正权最短路：dijkstra:")
     adj = {1:[(2,1),(3,3),(5,5)],2:[(4,5)],3:[(4,2),(6,7)],4:[(6,1)],5:[],6:[]}
     print(visualize(adj))
+    print("正权最短路：dijkstra:")
     dis = dijkstra(adj,1)
     print(dis)
     #负权最短路：SFPA
-    print("负权最短路：SFPA")
     adj = {1:[(2,1),(3,-3),(5,5)],2:[(3,-3),(4,5)],3:[(4,-2),(6,-7)],4:[(6,1)],5:[],6:[]}
     print(visualize(adj))
+    print("负权最短路：SFPA")
     dis = SPFA(adj,1)
     print(dis)
     #负权最短路：dijkstra(错误结果)
-    print("负权最短路：dijkstra(错误结果)")
     adj = {1:[(2,1),(3,-3),(5,5)],2:[(3,-3),(4,5)],3:[(4,-2),(6,-7)],4:[(6,1)],5:[],6:[(2,-10)]}
     print(visualize(adj))
+    print("负权最短路：dijkstra(错误结果)")
     dis = dijkstra(adj,1)
     print(dis)
     #负环检测：SFPA
     print("负环检测：SFPA")
-    adj = {1:[(2,1),(3,-3),(5,5)],2:[(3,-3),(4,5)],3:[(4,-2),(6,-7)],4:[(6,1)],5:[],6:[(2,-10)]}
-    print(visualize(adj))
     dis = SPFA(adj,1)
     print(dis)
