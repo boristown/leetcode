2cd27845aa8e8361427f6f9241067c7091e12d23
Boris Town <boristown@outlook.com>
2022-04-30 13:16:15 +0800

    Update _test_leetcode.py

--- _test_leetcode.py
+++ _test_leetcode.py
@@ -3,14 +3,14 @@ from Graph.ShortestPath.Dijkstra import dijkstra
 from Graph.visualize import visualize
 
 if __name__ == "__main__":
+    #
     print("re:")
     pat = "^(.+?\D)?0\d"
     print(not re.match(pat,"1+051"))
     print(not re.match(pat,"05+1"))
-
-if __name__ == '__main__':
+    #
     print("dijkstra:")
-    e = {1:[(2,1),(3,3)],2:[(4,5)],3:[(4,2)],4:[]}
+    e = {1:[(2,1),(3,3),(5,5)],2:[(4,5)],3:[(4,2),(6,7)],4:[(6,1)],5:[],6:[]}
     print(visualize(e))
     dis = dijkstra(e,1)
     print(dis)
