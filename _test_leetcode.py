900ddddb8087a4b52b89f4fb60850d3337f69937
Boris Town <boristown@outlook.com>
2022-04-30 13:38:15 +0800

    Update _test_leetcode.py

--- _test_leetcode.py
+++ _test_leetcode.py
@@ -3,11 +3,13 @@ from Graph.ShortestPath.Dijkstra import dijkstra
 from Graph.visualize import visualize
 
 if __name__ == "__main__":
-    #模式匹配：正则表达式
-    print("模式匹配：正则表达式:")
+    #模式匹配：邮箱
+    print("模式匹配：邮箱:")
     s = "boristown@gmail.com"
     pat = "^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
     print("字符串："+s+"\n正则："+pat+"\n匹配结果："+str(re.match(pat,s)))
+    #模式匹配：域名
+    print("模式匹配：域名:")
     s = "leetcode.cn"
     pat = "[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?"
     print("字符串："+s+"\n正则："+pat+"\n匹配结果："+str(re.match(pat,s)))
