bf68b97c9ab3986bcc1e629f76e1a35eae10063c
Boris Town <boristown@outlook.com>
2022-05-01 22:35:02 +0800

    Update pair_cnt.py

--- O(n) on array/pair_cnt.py
+++ O(n) on array/pair_cnt.py
@@ -1,8 +1,19 @@
 #值相等的元素对(i,j)的数量
 #https://codeforces.com/problemset/problem/50/B
+#*1500
+from collections import *
 
 def pair_cnt(L):
     '''
     找出L中值相等的元素对的数量
     '''
-    pass
+    #分析：
+    #观察长度n的列表增加为长度n+1后，元素对的变化
+    #假设增加的元素为e,且前n个元素中有m个e
+    #那么F(n+1) = F(n) + m * 2 + 1
+    C = Counter()
+    ans = 0
+    for i,a in enumerate(L):
+        ans += C[a]*2+1
+        C[a]+=1
+    return ans
