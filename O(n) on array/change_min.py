7205f4cb8f28ff440d436891a3fdaee6380b9f1e
Boris Town <boristown@outlook.com>
2022-05-02 14:51:45 +0800

    Update change_min.py

--- O(n) on array/change_min.py
+++ O(n) on array/change_min.py
@@ -1,7 +1,8 @@
 # 在区间内更改最少的元素，使得它不包含长度2以上的回文
 # 输入包括一系列区间查询，输出变更区间的最小成本
-# 字符串只含有a\b\c 
+# 字符串只含有a|b|c 
 #https://codeforces.com/problemset/problem/1555/D
+#*1600
 
 def change_min_palindrome(s,L):
     pass
