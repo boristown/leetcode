00818c92c25f8749db5d26ae8ea0c8df48c97664
Boris Town <boristown@outlook.com>
2022-05-03 12:49:34 +0800

    Update mysort.py

--- simple/mysort.py
+++ simple/mysort.py
@@ -1,5 +1,4 @@
 #自定义排序器
-#返回排序后的序列（元素，原顺序）
 def mykey(i,a):
     '''
     自定义排序key
@@ -12,6 +11,19 @@ def mykey(i,a):
     return (i,a)
 
 def mysort(L):
+    '''
+    自定义排序器
+    返回排序后的序列
+    '''
+    Evt = [(mykey(i,a),i,a) for i,a in enumerate(L)]
+    Evt.sort()
+    return [a for k,i,a in Evt]
+
+def mysort2(L):
+    '''
+    自定义排序器
+    返回排序后的序列（元素，原顺序）
+    '''
     Evt = [(mykey(i,a),i,a) for i,a in enumerate(L)]
     Evt.sort()
     return [(a,i) for k,i,a in Evt]
