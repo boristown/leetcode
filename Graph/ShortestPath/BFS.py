6e24e25ca1e5733c8633c6ef75051726a69d937a
AI Era <47687653+boristown@users.noreply.github.com>
2022-05-07 20:06:13 +0800

    最短路支持函数图

--- Graph/ShortestPath/BFS.py
+++ Graph/ShortestPath/BFS.py
@@ -86,3 +86,63 @@ def ShortestPath_random(graph,start,end):
                     return ans[::-1]
                 Q.append((step,node2))
     return ans[::-1]
+
+def ShortestPathLenF(graph,start,end):
+    '''
+    有向无权函数图中：从start到end的最短路长度
+    '''
+    if start == end: return 0
+    Q = deque([(0,start)])
+    vis = set([start])
+    while Q:
+        step,node = Q.popleft()
+        step += 1
+        for node2 in graph(node):
+            if node2 not in vis:
+                vis.add(node2)
+                if node2 == end:
+                    return step
+                Q.append((step,node2))
+    return -1
+    
+def ShortestPathF(graph,start,end):
+    '''
+    有向无权函数图中：从start到end的最短路
+    '''
+    if start == end: return [start]
+    last = {}
+    Q = deque([(0,start)])
+    vis = set([start])
+    ans = []
+    while Q:
+        step,node = Q.popleft()
+        step += 1
+        for node2 in graph(node):
+            if node2 not in vis:
+                last[node2] = node
+                vis.add(node2)
+                if node2 == end:
+                    ans.append(end)
+                    while node2 in last:
+                        ans.append(last[node2])
+                        node2 = last[node2]
+                    return ans[::-1]
+                Q.append((step,node2))
+    return ans[::-1]
+
+def ShortestPathALLF(graph,start):
+    '''
+    有向无权函数图中：从start到所有节点的最短路长度
+    '''
+    Q = deque([(0,start)])
+    vis = set([start])
+    ans = {start:0}
+    while Q:
+        step,node = Q.popleft()
+        step += 1
+        for node2 in graph(node):
+            if node2 not in vis:
+                ans[node2] = step
+                vis.add(node2)
+                Q.append((step,node2))
+    return ans
--- Graph/ShortestPath/Dijkstra.py
+++ Graph/ShortestPath/Dijkstra.py
@@ -27,3 +27,25 @@ def dijkstra(e,s):
                 dis[v] = dis[u] + w
                 heapq.heappush(q,(dis[v],v))
     return dis
+
+def dijkstraF(e,s):
+    '''
+    输入：
+    e:邻接函数表
+    s:起点
+    返回：
+    dis:从s到每个顶点的最短路长度
+    '''
+    dis = defaultdict(lambda:float("inf"))
+    dis[s] = 0
+    q = [(0,s)]
+    vis = set()
+    while q:
+        _, u = heapq.heappop(q)
+        if u in vis: continue
+        vis.add(u)
+        for v,w in e(u):
+            if dis[v] > dis[u] + w:
+                dis[v] = dis[u] + w
+                heapq.heappush(q,(dis[v],v))
+    return dis
