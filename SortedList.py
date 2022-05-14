0bc7f1298c1f8ffc6848ede2f0711609825e1e61
Boris Town <boristown@gmail.com>
2022-05-14 19:51:59 +0800

    SortedList

--- SortedList.py
+++ SortedList.py
@@ -904,17 +904,10 @@ class SortedList(MutableSequence):
 
     @recursive_repr()
     def __repr__(self):
-        """Return string representation of sorted list.
-        ``sl.__repr__()`` <==> ``repr(sl)``
-        :return: string representation
-        """
         return '{0}({1!r})'.format(type(self).__name__, list(self))
 
 
     def _check(self):
-        """Check invariants of sorted list.
-        Runtime complexity: `O(n)`
-        """
         try:
             assert self._load >= 4
             assert len(self._maxes) == len(self._lists)
@@ -971,18 +964,583 @@ class SortedList(MutableSequence):
                         assert child_sum == self._index[pos]
         except:
             traceback.print_exc(file=sys.stdout)
-            print('len', self._len)
-            print('load', self._load)
-            print('offset', self._offset)
-            print('len_index', len(self._index))
-            print('index', self._index)
-            print('len_maxes', len(self._maxes))
-            print('maxes', self._maxes)
-            print('len_lists', len(self._lists))
-            print('lists', self._lists)
             raise
 
 
 def identity(value):
-    "Identity function."
     return value
+
+class SortedKeyList(SortedList):
+    def __init__(self, iterable=None, key=identity):
+        self._key = key
+        self._len = 0
+        self._load = self.DEFAULT_LOAD_FACTOR
+        self._lists = []
+        self._keys = []
+        self._maxes = []
+        self._index = []
+        self._offset = 0
+
+        if iterable is not None:
+            self._update(iterable)
+
+
+    def __new__(cls, iterable=None, key=identity):
+        return object.__new__(cls)
+
+
+    @property
+    def key(self):
+        return self._key
+
+
+    def clear(self):
+        self._len = 0
+        del self._lists[:]
+        del self._keys[:]
+        del self._maxes[:]
+        del self._index[:]
+
+    _clear = clear
+
+
+    def add(self, value):
+        _lists = self._lists
+        _keys = self._keys
+        _maxes = self._maxes
+
+        key = self._key(value)
+
+        if _maxes:
+            pos = bisect_right(_maxes, key)
+
+            if pos == len(_maxes):
+                pos -= 1
+                _lists[pos].append(value)
+                _keys[pos].append(key)
+                _maxes[pos] = key
+            else:
+                idx = bisect_right(_keys[pos], key)
+                _lists[pos].insert(idx, value)
+                _keys[pos].insert(idx, key)
+
+            self._expand(pos)
+        else:
+            _lists.append([value])
+            _keys.append([key])
+            _maxes.append(key)
+
+        self._len += 1
+
+
+    def _expand(self, pos):
+        _lists = self._lists
+        _keys = self._keys
+        _index = self._index
+
+        if len(_keys[pos]) > (self._load << 1):
+            _maxes = self._maxes
+            _load = self._load
+
+            _lists_pos = _lists[pos]
+            _keys_pos = _keys[pos]
+            half = _lists_pos[_load:]
+            half_keys = _keys_pos[_load:]
+            del _lists_pos[_load:]
+            del _keys_pos[_load:]
+            _maxes[pos] = _keys_pos[-1]
+
+            _lists.insert(pos + 1, half)
+            _keys.insert(pos + 1, half_keys)
+            _maxes.insert(pos + 1, half_keys[-1])
+
+            del _index[:]
+        else:
+            if _index:
+                child = self._offset + pos
+                while child:
+                    _index[child] += 1
+                    child = (child - 1) >> 1
+                _index[0] += 1
+
+
+    def update(self, iterable):
+        _lists = self._lists
+        _keys = self._keys
+        _maxes = self._maxes
+        values = sorted(iterable, key=self._key)
+
+        if _maxes:
+            if len(values) * 4 >= self._len:
+                _lists.append(values)
+                values = reduce(iadd, _lists, [])
+                values.sort(key=self._key)
+                self._clear()
+            else:
+                _add = self.add
+                for val in values:
+                    _add(val)
+                return
+
+        _load = self._load
+        _lists.extend(values[pos:(pos + _load)]
+                      for pos in range(0, len(values), _load))
+        _keys.extend(list(map(self._key, _list)) for _list in _lists)
+        _maxes.extend(sublist[-1] for sublist in _keys)
+        self._len = len(values)
+        del self._index[:]
+
+    _update = update
+
+
+    def __contains__(self, value):
+        _maxes = self._maxes
+
+        if not _maxes:
+            return False
+
+        key = self._key(value)
+        pos = bisect_left(_maxes, key)
+
+        if pos == len(_maxes):
+            return False
+
+        _lists = self._lists
+        _keys = self._keys
+
+        idx = bisect_left(_keys[pos], key)
+
+        len_keys = len(_keys)
+        len_sublist = len(_keys[pos])
+
+        while True:
+            if _keys[pos][idx] != key:
+                return False
+            if _lists[pos][idx] == value:
+                return True
+            idx += 1
+            if idx == len_sublist:
+                pos += 1
+                if pos == len_keys:
+                    return False
+                len_sublist = len(_keys[pos])
+                idx = 0
+
+
+    def discard(self, value):
+        _maxes = self._maxes
+
+        if not _maxes:
+            return
+
+        key = self._key(value)
+        pos = bisect_left(_maxes, key)
+
+        if pos == len(_maxes):
+            return
+
+        _lists = self._lists
+        _keys = self._keys
+        idx = bisect_left(_keys[pos], key)
+        len_keys = len(_keys)
+        len_sublist = len(_keys[pos])
+
+        while True:
+            if _keys[pos][idx] != key:
+                return
+            if _lists[pos][idx] == value:
+                self._delete(pos, idx)
+                return
+            idx += 1
+            if idx == len_sublist:
+                pos += 1
+                if pos == len_keys:
+                    return
+                len_sublist = len(_keys[pos])
+                idx = 0
+
+
+    def remove(self, value):
+        _maxes = self._maxes
+
+        if not _maxes:
+            raise ValueError('{0!r} not in list'.format(value))
+
+        key = self._key(value)
+        pos = bisect_left(_maxes, key)
+
+        if pos == len(_maxes):
+            raise ValueError('{0!r} not in list'.format(value))
+
+        _lists = self._lists
+        _keys = self._keys
+        idx = bisect_left(_keys[pos], key)
+        len_keys = len(_keys)
+        len_sublist = len(_keys[pos])
+
+        while True:
+            if _keys[pos][idx] != key:
+                raise ValueError('{0!r} not in list'.format(value))
+            if _lists[pos][idx] == value:
+                self._delete(pos, idx)
+                return
+            idx += 1
+            if idx == len_sublist:
+                pos += 1
+                if pos == len_keys:
+                    raise ValueError('{0!r} not in list'.format(value))
+                len_sublist = len(_keys[pos])
+                idx = 0
+
+
+    def _delete(self, pos, idx):
+        _lists = self._lists
+        _keys = self._keys
+        _maxes = self._maxes
+        _index = self._index
+        keys_pos = _keys[pos]
+        lists_pos = _lists[pos]
+
+        del keys_pos[idx]
+        del lists_pos[idx]
+        self._len -= 1
+
+        len_keys_pos = len(keys_pos)
+
+        if len_keys_pos > (self._load >> 1):
+            _maxes[pos] = keys_pos[-1]
+
+            if _index:
+                child = self._offset + pos
+                while child > 0:
+                    _index[child] -= 1
+                    child = (child - 1) >> 1
+                _index[0] -= 1
+        elif len(_keys) > 1:
+            if not pos:
+                pos += 1
+
+            prev = pos - 1
+            _keys[prev].extend(_keys[pos])
+            _lists[prev].extend(_lists[pos])
+            _maxes[prev] = _keys[prev][-1]
+
+            del _lists[pos]
+            del _keys[pos]
+            del _maxes[pos]
+            del _index[:]
+
+            self._expand(prev)
+        elif len_keys_pos:
+            _maxes[pos] = keys_pos[-1]
+        else:
+            del _lists[pos]
+            del _keys[pos]
+            del _maxes[pos]
+            del _index[:]
+
+
+    def irange(self, minimum=None, maximum=None, inclusive=(True, True),
+               reverse=False):
+        min_key = self._key(minimum) if minimum is not None else None
+        max_key = self._key(maximum) if maximum is not None else None
+        return self._irange_key(
+            min_key=min_key, max_key=max_key,
+            inclusive=inclusive, reverse=reverse,
+        )
+
+
+    def irange_key(self, min_key=None, max_key=None, inclusive=(True, True),
+                   reverse=False):
+        _maxes = self._maxes
+
+        if not _maxes:
+            return iter(())
+
+        _keys = self._keys
+
+        # Calculate the minimum (pos, idx) pair. By default this location
+        # will be inclusive in our calculation.
+
+        if min_key is None:
+            min_pos = 0
+            min_idx = 0
+        else:
+            if inclusive[0]:
+                min_pos = bisect_left(_maxes, min_key)
+
+                if min_pos == len(_maxes):
+                    return iter(())
+
+                min_idx = bisect_left(_keys[min_pos], min_key)
+            else:
+                min_pos = bisect_right(_maxes, min_key)
+
+                if min_pos == len(_maxes):
+                    return iter(())
+
+                min_idx = bisect_right(_keys[min_pos], min_key)
+
+        # Calculate the maximum (pos, idx) pair. By default this location
+        # will be exclusive in our calculation.
+
+        if max_key is None:
+            max_pos = len(_maxes) - 1
+            max_idx = len(_keys[max_pos])
+        else:
+            if inclusive[1]:
+                max_pos = bisect_right(_maxes, max_key)
+
+                if max_pos == len(_maxes):
+                    max_pos -= 1
+                    max_idx = len(_keys[max_pos])
+                else:
+                    max_idx = bisect_right(_keys[max_pos], max_key)
+            else:
+                max_pos = bisect_left(_maxes, max_key)
+
+                if max_pos == len(_maxes):
+                    max_pos -= 1
+                    max_idx = len(_keys[max_pos])
+                else:
+                    max_idx = bisect_left(_keys[max_pos], max_key)
+
+        return self._islice(min_pos, min_idx, max_pos, max_idx, reverse)
+
+    _irange_key = irange_key
+
+
+    def bisect_left(self, value):
+        return self._bisect_key_left(self._key(value))
+
+
+    def bisect_right(self, value):
+        return self._bisect_key_right(self._key(value))
+
+    bisect = bisect_right
+
+
+    def bisect_key_left(self, key):
+        _maxes = self._maxes
+
+        if not _maxes:
+            return 0
+
+        pos = bisect_left(_maxes, key)
+
+        if pos == len(_maxes):
+            return self._len
+
+        idx = bisect_left(self._keys[pos], key)
+
+        return self._loc(pos, idx)
+
+    _bisect_key_left = bisect_key_left
+
+
+    def bisect_key_right(self, key):
+        _maxes = self._maxes
+
+        if not _maxes:
+            return 0
+
+        pos = bisect_right(_maxes, key)
+
+        if pos == len(_maxes):
+            return self._len
+
+        idx = bisect_right(self._keys[pos], key)
+
+        return self._loc(pos, idx)
+
+    bisect_key = bisect_key_right
+    _bisect_key_right = bisect_key_right
+
+
+    def count(self, value):
+        _maxes = self._maxes
+
+        if not _maxes:
+            return 0
+
+        key = self._key(value)
+        pos = bisect_left(_maxes, key)
+
+        if pos == len(_maxes):
+            return 0
+
+        _lists = self._lists
+        _keys = self._keys
+        idx = bisect_left(_keys[pos], key)
+        total = 0
+        len_keys = len(_keys)
+        len_sublist = len(_keys[pos])
+
+        while True:
+            if _keys[pos][idx] != key:
+                return total
+            if _lists[pos][idx] == value:
+                total += 1
+            idx += 1
+            if idx == len_sublist:
+                pos += 1
+                if pos == len_keys:
+                    return total
+                len_sublist = len(_keys[pos])
+                idx = 0
+
+
+    def copy(self):
+        return self.__class__(self, key=self._key)
+
+    __copy__ = copy
+
+
+    def index(self, value, start=None, stop=None):
+        _len = self._len
+
+        if not _len:
+            raise ValueError('{0!r} is not in list'.format(value))
+
+        if start is None:
+            start = 0
+        if start < 0:
+            start += _len
+        if start < 0:
+            start = 0
+
+        if stop is None:
+            stop = _len
+        if stop < 0:
+            stop += _len
+        if stop > _len:
+            stop = _len
+
+        if stop <= start:
+            raise ValueError('{0!r} is not in list'.format(value))
+
+        _maxes = self._maxes
+        key = self._key(value)
+        pos = bisect_left(_maxes, key)
+
+        if pos == len(_maxes):
+            raise ValueError('{0!r} is not in list'.format(value))
+
+        stop -= 1
+        _lists = self._lists
+        _keys = self._keys
+        idx = bisect_left(_keys[pos], key)
+        len_keys = len(_keys)
+        len_sublist = len(_keys[pos])
+
+        while True:
+            if _keys[pos][idx] != key:
+                raise ValueError('{0!r} is not in list'.format(value))
+            if _lists[pos][idx] == value:
+                loc = self._loc(pos, idx)
+                if start <= loc <= stop:
+                    return loc
+                elif loc > stop:
+                    break
+            idx += 1
+            if idx == len_sublist:
+                pos += 1
+                if pos == len_keys:
+                    raise ValueError('{0!r} is not in list'.format(value))
+                len_sublist = len(_keys[pos])
+                idx = 0
+
+        raise ValueError('{0!r} is not in list'.format(value))
+
+
+    def __add__(self, other):
+        values = reduce(iadd, self._lists, [])
+        values.extend(other)
+        return self.__class__(values, key=self._key)
+
+    __radd__ = __add__
+
+
+    def __mul__(self, num):
+        values = reduce(iadd, self._lists, []) * num
+        return self.__class__(values, key=self._key)
+
+
+    def __reduce__(self):
+        values = reduce(iadd, self._lists, [])
+        return (type(self), (values, self.key))
+
+
+    @recursive_repr()
+    def __repr__(self):
+        type_name = type(self).__name__
+        return '{0}({1!r}, key={2!r})'.format(type_name, list(self), self._key)
+
+
+    def _check(self):
+        try:
+            assert self._load >= 4
+            assert len(self._maxes) == len(self._lists) == len(self._keys)
+            assert self._len == sum(len(sublist) for sublist in self._lists)
+
+            # Check all sublists are sorted.
+
+            for sublist in self._keys:
+                for pos in range(1, len(sublist)):
+                    assert sublist[pos - 1] <= sublist[pos]
+
+            # Check beginning/end of sublists are sorted.
+
+            for pos in range(1, len(self._keys)):
+                assert self._keys[pos - 1][-1] <= self._keys[pos][0]
+
+            # Check _keys matches _key mapped to _lists.
+
+            for val_sublist, key_sublist in zip(self._lists, self._keys):
+                assert len(val_sublist) == len(key_sublist)
+                for val, key in zip(val_sublist, key_sublist):
+                    assert self._key(val) == key
+
+            # Check _maxes index is the last value of each sublist.
+
+            for pos in range(len(self._maxes)):
+                assert self._maxes[pos] == self._keys[pos][-1]
+
+            # Check sublist lengths are less than double load-factor.
+
+            double = self._load << 1
+            assert all(len(sublist) <= double for sublist in self._lists)
+
+            # Check sublist lengths are greater than half load-factor for all
+            # but the last sublist.
+
+            half = self._load >> 1
+            for pos in range(0, len(self._lists) - 1):
+                assert len(self._lists[pos]) >= half
+
+            if self._index:
+                assert self._len == self._index[0]
+                assert len(self._index) == self._offset + len(self._lists)
+
+                # Check index leaf nodes equal length of sublists.
+
+                for pos in range(len(self._lists)):
+                    leaf = self._index[self._offset + pos]
+                    assert leaf == len(self._lists[pos])
+
+                # Check index branch nodes are the sum of their children.
+
+                for pos in range(self._offset):
+                    child = (pos << 1) + 1
+                    if child >= len(self._index):
+                        assert self._index[pos] == 0
+                    elif child + 1 == len(self._index):
+                        assert self._index[pos] == self._index[child]
+                    else:
+                        child_sum = self._index[child] + self._index[child + 1]
+                        assert child_sum == self._index[pos]
+        except:
+            traceback.print_exc(file=sys.stdout)
+            raise
+
+SortedListWithKey = SortedKeyList
