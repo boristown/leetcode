040ed68043216d78848128599374e48a7498da4d
Boris Town <boristown@gmail.com>
2022-05-28 12:41:16 +0800

    添加了括号处理的变形逻辑

--- bracket_range.py
+++ bracket_range.py
@@ -2,22 +2,19 @@
 
 class bracket_range:
     def __init__(self,s):
-        p1,p2,self.psum1,self.psum2 = 0,0,[0],[0]
-        for a in s:
-            if a == '(':
-                p1+=1
-            else:
-                p2+=1
-            self.psum1.append(p1)
-            self.psum2.append(p2)
+        self.s = s
         
     def get_range(self,i,j):
         '''
         correct bracket length in [i,j)
         '''
-        s1 = self.psum1[j]-self.psum1[i]
-        s2 = self.psum2[j]-self.psum2[i]
-        ans = min(s1,s2)
+        ans,b = 0,0
+        for p in range(i,j):
+            a = self.s[p]
+            if a == '(':
+                b+=1
+            else:
+                ans = min(ans + 1, b)
         return ans*2
 
 def _bracket(s,i,j):
@@ -32,12 +29,34 @@ def _bracket(s,i,j):
             if b>0:
                 b-=1
             ans+=1
+    ans*=2
     #2. 变形：O(n)
-    ans,b = 0,0 #答案, 上限
+    ans,b = 0,0
     for p in range(i,j):
         a = s[p]
         if a == '(':
             b+=1
         else:
             ans = min(ans + 1, b)
+    ans*=2
+    #3. 再变形: O(n)
+    ans,b = 0,0
+    for p in range(i,j):
+        a = s[p]
+        b = psum[p+1]-psum[i]
+        if a == ')':
+            ans = min(ans + 1, b)
+    ans*=2
+    #4. 再变形: O(n)
+    ans = psum[i]
+    for p in range(i,j):
+        if s[p] == ')':
+            ans = min(ans + 1, psum[p+1])
+    ans = (ans - psum[i])*2
+    #5. 再变形: O(n)
+    ans = psum[i]
+    for p in range(i,j):
+        if s[p] == ')' and ans < psum[p+1]:
+            ans += 1
+    ans = (ans - psum[i])*2
     return ans
--- main_codeforces.py
+++ main_codeforces.py
@@ -18,22 +18,19 @@
 
 class bracket_range:
     def __init__(self,s):
-        p1,p2,self.psum1,self.psum2 = 0,0,[0],[0]
-        for a in s:
-            if a == '(':
-                p1+=1
-            else:
-                p2+=1
-            self.psum1.append(p1)
-            self.psum2.append(p2)
-    
+        self.s = s
+        
     def get_range(self,i,j):
         '''
         correct bracket length in [i,j)
         '''
-        s1 = self.psum1[j]-self.psum1[i]
-        s2 = self.psum2[j]-self.psum2[i]
-        ans = min(s1,s2)
+        ans,b = 0,0
+        for p in range(i,j):
+            a = self.s[p]
+            if a == '(':
+                b+=1
+            else:
+                ans = min(ans + 1, b)
         return ans*2
 
 s = input() #input string
