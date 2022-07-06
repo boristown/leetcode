68f06a27c5c88b0d4ae89c49ea1540b05f5df4ca
AI Era <47687653+boristown@users.noreply.github.com>
2022-07-06 17:44:41 +0800

    Update main_codeforces.py

--- main_codeforces.py
+++ main_codeforces.py
@@ -17,37 +17,16 @@
 #end of codeforces template
 
 from collections import *
-import math
 
 inf = float("inf")
-
-def get_primes(n=100000):
-    flag = False
-    prime_number = [2]
-    for i in range(3, n, 2):
-        edge = math.ceil(math.sqrt(i))+1
-        for j in prime_number:
-            if i % j == 0:
-                flag = True
-                break
-            if j >= edge:
-                flag = False
-                break
-        if not flag:
-            prime_number.append(i)
-    return prime_number
-
-n = int(input())
-if n == 1:
-    print(1)
-    print(1)
-elif n == 2:
-    print(1)
-    print(1,1)
-else:
-    primes = set(get_primes(n+1))
-    print(2)
-    ans = []
-    for i in range(2,n+2):
-        ans.append("1" if i in primes else "2")
-    print(" ".join(ans))
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    L = list(map(int,input().split())) #input list
+    L.sort()
+    for i in range(n-1):
+        if L[i+1]-L[i]==1:
+            print("NO")
+            break
+    else:
+        print("YES")
