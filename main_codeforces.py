c428bba3d4296a17044b02061b448ec440998a8b
Boris Town <boristown@gmail.com>
2022-07-17 21:14:19 +0800

    Update main_codeforces.py

--- main_codeforces.py
+++ main_codeforces.py
@@ -18,35 +18,13 @@
 
 from collections import *
 
-def issorted(A):
-    n = len(A)
-    for i in range(n-1):
-        if A[i]>A[i+1]:
-            return False
-    return True
-    
-def solve(n,x,L):
-    if x*2<=n:
-        return "YES"
-    if n == x:
-        return "YES" if issorted(L) else "NO"
-    l=n-x
-    r=x-1
-    B = L[l:r+1]
-    if not issorted(B):
-        return 'NO'
-    A = L[:l] + L[r+1:]
-    A.sort()
-    l0,r0 = A[l-1],A[l]
-    l1,r1 = L[l],L[r]
-    if l0<=l1 <= r1 <= r0:
-        return "YES"
-    return "NO"
-
 inf = float("inf")
-t = int(input())
-for _ in range(t):
-    n,x = map(int,input().split()) #input tuple
+
+t = int(input()) #input number of test cases
+for _ in range(t): #iter for test cases
+    n = int(input()) #input int
     L = list(map(int,input().split())) #input list
-    ans = solve(n,x,L)
-    print(ans)
+    if L[0] < L [-1]:
+        print("YES")
+    else:
+        print("NO")
