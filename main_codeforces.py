9225dae3cc3259c2fc92ded59d0378f40daf2aa6
Boris Town <boristown@gmail.com>
2022-07-19 22:47:31 +0800

    Update main_codeforces.py

--- main_codeforces.py
+++ main_codeforces.py
@@ -17,26 +17,27 @@
 #end of codeforces template
 
 from collections import *
+import math
 
 inf = float("inf")
 
 t = int(input()) #input number of test cases
 for _ in range(t): #iter for test cases
-    n = int(input()) #input int
-    L = list(map(int,input().split())) #input list
-    def solve(i):
-        if i >= n-1:
-            return 0
-        return max(max(L[i-1],L[i+1])+1-L[i],0)
-    dp = [0]*(n+1)
-    for i in range(n-2,0,-2):
-        dp[i] = solve(i) + dp[i+2]
-    def F(n,j):
-        if n < 3: return 0
-        if n%2==1:
-            return dp[j+1]
-        else:
-            ans1 = solve(j+1) + F(n-2,j+2)
-            ans2 = solve(j+2) + F(n-3,j+3)
-            return min(ans1,ans2)
-    print(F(n,0))
+    n,k = map(int,input().split()) #input tuple
+    A = list(map(int,input().split())) #input list
+    A.sort(reverse=True)
+    ans = inf
+    mx,mn = A[0],A[-1]//k
+    for a in range(max(1,mn),mx+1):
+        mna,mxa = inf,-inf
+        for i in range(n):
+            d = A[i]/a
+            d1,d2 = max(1,min(k,math.ceil(d))),min(k,max(math.floor(d),1))
+            c1,c2 = A[i]//d1,A[i]//d2
+            if abs(c1-a)>abs(c2-a):
+                c1,c2=c2,c1
+            mna,mxa = min(mna,c1),max(mxa,c1)
+        an = mxa-mna
+        if an < ans:
+            ans = an
+    print(ans)
