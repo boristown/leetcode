86c6d84e69fcd5b721659d35daae34e861c60c8b
Boris Town <boristown@gmail.com>
2022-07-31 21:41:11 +0800

    Update main_codeforces.py

--- main_codeforces.py
+++ main_codeforces.py
@@ -18,23 +18,32 @@
 #end of codeforces template
 
 from collections import *
+from functools import *
 import math
 
 inf = float("inf")
 
-def solve(n,m,k,A):
-    if A[0] >= nm:
-        return True
-    B = [a//n for a in A]
-    C = [b//2*2 for b in B if b > 1]
-    return sum(C) >= m
+MOD = 998244353
 
-t = int(input()) #input number of test cases
-for _ in range(t): #iter for test cases
-    n,m,k = map(int,input().split()) #input tuple
-    A = list(map(int,input().split())) #input list
-    A.sort(reverse=True)
-    nm = n*m
-    ans = A[0] >= nm or solve(n,m,k,A) or solve(m,n,k,A)
-    if ans: print("Yes")
-    else: print("No")
+n = int(input()) #input number of test cases
+A = list(map(int,input().split())) #input list
+ans = 0
+
+def dp(i,a,b,c):
+    if a == 0:
+        if c == 0:
+            return 1
+        else:
+            return 0
+    n1 = n - i
+    if n1 < a:
+        return 0
+    r = A[i]%b
+    c2=(c+r)%b
+    ans1 = dp(i+1,a-1,b,c2)
+    ans2 = dp(i+1,a,b,c)
+    return ans1+ans2
+    
+for a in range(1,n+1):
+    ans += dp(0,a,a,0)
+print(ans%MOD)
