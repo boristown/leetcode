4c2e76e461ed4530383fb4379b4b441ccfba6e7d
Boris Town <boristown@gmail.com>
2022-08-05 00:42:04 +0800

    Update main_codeforces.py

--- main_codeforces.py
+++ main_codeforces.py
@@ -18,48 +18,49 @@
 #end of codeforces template
 
 from collections import *
-from functools import *
-import math
 
 inf = float("inf")
 
-def solve(s,n,S):
-    m = len(s)
-    M = {}
-    for idx,c in enumerate(S):
-        l = len(c)
-        for i in range(m-l+1):
-            if c == s[i:i+l]:
-                for pos in range(i,i+l):
-                    le2 = l-pos+i
-                    if pos not in M:
-                        M[pos] = (idx+1,i+1,le2)
-                    else:
-                        _,_,le = M[pos]
-                        if le2 > le:
-                            M[pos] = (idx+1,i+1,le2)
-    ans = []
-    p = 0
-    while p<m:
-        if p in M:
-            a,b,c = M[p]
-            ans.append((a,b))
-            p+=c
-        else:
-            return []
+def solve(m,A):
+    M = [(1,0),(0,1),(-1,0),(0,1)]
+    dp = [[0]*(m+1) for _ in range(2)]
+    dp[0][-1] = 1
+    dp[1][-1] = 1
+    k = 1
+    for j in range(m-1,-1,-1):
+        dp[0][j] = max(A[0][j],A[1][j]+k,dp[1][j+1]+1)
+        dp[1][j] = max(A[0][j]+k,A[1][j],dp[0][j+1]+1)
+        k+=2
+    ans2 = inf
+    ans = 0
+    i,j = 0,0
+    for k in range(m*2-1):
+        if (j-i) % 2== 0:
+            ans2 = min(ans2,max(ans,dp[i][j]+1))
+        ans+=1
+        x,y = M[k%4]
+        i,j = i+x,j+y
+        if ans <= A[i][j]:
+            ans = A[i][j]+1
+    return min(ans,ans2)
+
+def solve3(m,A):
+    ans = 0
+    for j in range(0,m):
+        ans+=1
+        if ans <= A[1][j]:
+            ans = A[1][j]+1
+    for j in range(m-1,0,-1):
+        ans+=1
+        if ans <= A[0][j]:
+            ans = A[0][j]+1
     return ans
     
 t = int(input()) #input number of test cases
 for _ in range(t): #iter for test cases
-    s = input()
-    n = int(input()) #input int
-    S = []
-    for _ in range(n):
-        S.append(input())
-    ans = solve(s,n,S)
-    if not ans:
-        print(-1)
-    else:
-        print(len(ans))
-        for a,b in ans:
-            print(a,b)
+    m = int(input()) #input int
+    L1 = list(map(int,input().split())) #input list
+    L2 = list(map(int,input().split())) #input list
+    A = [L1,L2]
+    ans = solve(m,A)
+    print(ans)
