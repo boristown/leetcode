38cc769345c2b03b2bdad1bbc044812bc21652cd
Boris Town <boristown@gmail.com>
2022-08-12 23:27:08 +0800

    upd

--- main_codeforces.cpp
+++ main_codeforces.cpp
@@ -97,48 +97,24 @@ typedef long long LL;
 
 using namespace std;
 
-class Primes{
-    public: 
-    const static int N = 1e8+10;
-    const static int M = 1e7+10;
-
-    public:
-    int cnt=0;
-    bool isPrime[N];
-    int result[M];
-
-    //2倍欧拉筛,找小于N的所有素数
-    public:
-    void get_primes_o(int n=100000){
-        memset(isPrime,1,N*sizeof(bool));
-        result[0] = 2;
-        int n2 = n / 2;
-        cnt = 1;
-        int i2 = 0;
-        for(int i=3;i<n;i+=2){
-            i2++;
-            if(isPrime[i2]){
-                result[cnt++]=i;
-            }
-            for(int j=1;j<cnt;j++){
-                int e = result[j];
-                int ei = e*i;
-                if(ei>=n) break;
-                isPrime[ei>>1] = 0;
-                if(i%e==0) break;
-            }
-        }
-    }
-};
-
 int main() {
-    int n,q,x;
-    cin>>n>>q;
-    auto pri = new Primes();
-    pri->get_primes_o(n+1);
-    REP(i,1,q){
-        cin>>x;
-        cout<<pri->result[x-1]<<endl;
+    int n;
+    int k,a;
+    cin>>n;
+    auto pq = priority_queue<int,vector<int>,greater<int>>();
+    REP(i,1,n)
+    {
+        cin>>k;
+        if(k==1){
+            cin>>a;
+            pq.push(a);
+        }
+        else if(k==2){
+            cout << pq.top() << endl;
+        }
+        else{
+            pq.pop();
+        }
     }
     return 0;
 };
--- main_codeforces.py
+++ main_codeforces.py
@@ -21,51 +21,14 @@ from collections import *
 
 inf = float("inf")
 
-X = dict()
-R = dict() # 各点的初始等级均为0,如果被做为连接的的末端，则增加1
-
-def make_set(point):
-    X[point] = point
-    R[point] = 0
-def find(point):
-    if X[point] != point:
-        X[point] = find(X[point])
-    return X[point]
-def merge(point1, point2):
-    '''连接两个分量（节点）
-    '''
-    r1 = find(point1)
-    r2 = find(point2)
-    if r1 != r2:
-        if R[r1] > R[r2]:
-            X[r2] = r1
-        else:
-            X[r1] = r2
-            if R[r1] == R[r2]:
-                R[r2] += 1
-
-def kruskal(vertices,edges):
-    '''KRUSKAL算法实现
-    '''
-    for vertice in vertices:
-        make_set(vertice)
-    minu_tree = []
-    edges.sort()  # 按照权重从小到大排序
-    for edge in edges:
-        weight, vertice1, vertice2 = edge
-        if find(vertice1) != find(vertice2):
-            merge(vertice1, vertice2)
-            minu_tree.append(edge)
-    return minu_tree
-
-n,m = map(int,input().split())
-vertices = [i+1 for i in range(n)]
-edges = []
-for _ in range(m):
-    x,y,z = map(int,input().split())
-    edges.append((z,x,y))
-ans = kruskal(vertices,edges)
-if len(ans) == n-1:
-    print(sum(x for x,_,_ in ans))
-else:
-    print("orz")
+from heapq import *
+t = int(input())
+hp = []
+for _ in range(t):
+    s = input()
+    if s[0] == '1':
+        heappush(hp,int(s[2:]))
+    elif s[0] == '2':
+        print(hp[0])
+    elif s[0] == '3':
+        heappop(hp)
