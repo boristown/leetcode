df0561037747078b2a6b900ffed474fcad5c5970
Boris Town <boristown@gmail.com>
2022-08-28 12:14:49 +0800

    拓扑排序器

--- Graph/TopologicalSortor.py
+++ Graph/TopologicalSortor.py
@@ -1,5 +1,6 @@
 from collections import *
 import collections
+from typing import *
 
 class TopologicalSortor:
     '''
@@ -47,7 +48,7 @@ class TopologicalSortor:
 
 class CircleDetect:
     def noCircle(self, numCourses, prerequisites) -> bool:
-        edges = Collection.defaultdict(list)
+        edges = collections.defaultdict(list)
         visited = [0] * numCourses
         result = list()
         valid = True
@@ -74,3 +75,37 @@ class CircleDetect:
                 dfs(i)
         
         return valid
+
+class TopologicalSortorWithCircle:
+    def solve(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        '''
+        带环路的拓扑排序器
+        '''
+        # 存储有向图
+        edges = collections.defaultdict(list)
+        # 存储每个节点的入度
+        indeg = [0] * numCourses
+        # 存储答案
+        result = list()
+
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+            indeg[info[0]] += 1
+        
+        # 将所有入度为 0 的节点放入队列中
+        q = collections.deque([u for u in range(numCourses) if indeg[u] == 0])
+
+        while q:
+            # 从队首取出一个节点
+            u = q.popleft()
+            # 放入答案中
+            result.append(u)
+            for v in edges[u]:
+                indeg[v] -= 1
+                # 如果相邻节点 v 的入度为 0，就可以选 v 对应的课程了
+                if indeg[v] == 0:
+                    q.append(v)
+
+        if len(result) != numCourses:
+            result = list()
+        return result
